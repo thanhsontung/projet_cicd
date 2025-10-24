d5f63613238e0a9723a3aa84519d8fbf853182b2
thanh_tung <sont48873@gmail.com>
2025-10-24 14:35:45 +0700

    update code 24

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
-const App = require("./src/app");
+// require("dotenv").config();
+// const App = require("./src/app");
 
-const app = new App();
-app.start();
+// const app = new App();
+// app.start();
